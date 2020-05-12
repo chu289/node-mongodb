332de5285f57e883b8003c43ca27f243dfdaa270
chu289 <61627506+chu289@users.noreply.github.com>
2020-05-13 00:27:07 +0800

    v1.01

--- config/database.config.js
+++ config/database.config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    url: 'mongodb://admin:Taiwan%402020@172.30.157.130:27017/sampledb'
+    url: 'mongodb://admin:Taiwan%402020@172.30.12.162:27017/sampledb'
     //url: 'mongodb://@localhost:27017/test'
 };
